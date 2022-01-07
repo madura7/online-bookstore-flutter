848779dc3408fa093ecfb9a253bd8aefee8f0028
Madhura <madura12a@gmail.com>
2022-01-08 01:16:17 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Book Store App
+
+- [My Linkedin](https://www.linkedin.com/in/madura7/)
+- [My Upwork](https://www.upwork.com/freelancers/~016c06a2957baddfb8)
+- [My Fiverr](https://www.fiverr.com/codemax7)
+
+- My Email: dev.madura@gmail.com
---
+++ lib/theme/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const primary = Color(0xFFFF6116);
+const secondary = Colors.white;
+const mainColor = Color(0xFF000000);
+const appBgColor = Color(0xFFF5F5F5);
+const bottomBarColor = Color(0xFFFF6116);
+const shadowColor = Colors.black87;
